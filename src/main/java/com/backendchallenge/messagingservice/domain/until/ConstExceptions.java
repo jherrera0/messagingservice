b05b02708d881260a01475949b0943ef4991fc09
Jose Herrera <jose.herrera@pragma.com.co>
2025-02-04 08:46:27 -0500

    implement logic of authentication on the domain layer

---
+++ src/main/java/com/backendchallenge/messagingservice/domain/until/ConstExceptions.java
@@ -0,0 +1,8 @@
+package com.backendchallenge.messagingservice.domain.until;
+
+public class ConstExceptions {
+    public static final String USER_NOT_FOUND = "User not found";
+
+    private ConstExceptions() {
+    }
+}
---
+++ src/main/java/com/backendchallenge/messagingservice/domain/until/ConstJwt.java
@@ -0,0 +1,13 @@
+package com.backendchallenge.messagingservice.domain.until;
+
+public class ConstJwt {
+    public static final String BEARER = "Bearer";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String ROLE = "Role";
+    public static final int LINESTRING_INDEX = 7;
+    public static final String ID = "Id";
+    public static final String HAS_AUTHORITY_EMPLOYEE = "hasAuthority('EMPLOYEE')";
+
+    private ConstJwt() {
+    }
+}
