43249a72c136d4fd6b31b10437f53e9ce62294cb
Jose Herrera <jose.herrera@pragma.com.co>
2025-02-04 15:06:00 -0500

    change logic of the infrastructure layer

--- src/main/java/com/backendchallenge/messagingservice/domain/until/ConstDocumentation.java
+++ src/main/java/com/backendchallenge/messagingservice/domain/until/ConstDocumentation.java
@@ -13,6 +13,17 @@ public class ConstDocumentation {
     public static final String CODE_200_DESCRIPTION_NOTIFICATION = "Notification sent successfully";
     public static final String CODE_400_DESCRIPTION_NOTIFICATION = "Invalid data to send notification";
     public static final String CODE_403_DESCRIPTION_NOTIFICATION = "User not authorized to send notifications";
+    public static final String EXIST_PIN_OPERATION_SUMMARY = "Check if a pin exists";
+    public static final String EXIST_PIN_OPERATION_DESCRIPTION = "Check if a pin exists for a phone number";
+    public static final String CODE_200_DESCRIPTION_EXIST_PIN = "Pin exists";
+    public static final String CODE_400_DESCRIPTION_EXIST_PIN = "Invalid data to check pin";
+    public static final String CODE_403_DESCRIPTION_EXIST_PIN = "User not authorized to check pin";
+    public static final String FIND_PIN_OPERATION_SUMMARY = "Find a pin";
+    public static final String FIND_PIN_OPERATION_DESCRIPTION = "Find a pin for a phone number";
+    public static final String CODE_200_DESCRIPTION_FIND_PIN = "Pin found";
+    public static final String CODE_400_DESCRIPTION_FIND_PIN = "Invalid data to find pin";
+    public static final String CODE_403_DESCRIPTION_FIND_PIN = "User not authorized to find pin";
+
 
     private ConstDocumentation() {
     }
