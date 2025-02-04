87a71948959955aff970a6f541cb7e6dae69ee19
Jose Herrera <jose.herrera@pragma.com.co>
2025-02-04 08:22:26 -0500

    Initial commit

---
+++ src/main/java/com/backendchallenge/messagingservice/MessagingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.backendchallenge.messagingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backendchallenge/messagingservice/MessagingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendchallenge.messagingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
