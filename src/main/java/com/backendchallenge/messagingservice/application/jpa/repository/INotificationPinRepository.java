dbd0ad5580da058b13e01a2c4ede40e8f9cfac2a
Jose Herrera <jose.herrera@pragma.com.co>
2025-02-05 14:28:41 -0500

    remove unused elements

--- src/main/java/com/backendchallenge/messagingservice/application/jpa/repository/INotificationPinRepository.java
+++ src/main/java/com/backendchallenge/messagingservice/application/jpa/repository/INotificationPinRepository.java
@@ -7,7 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface INotificationPinRepository extends JpaRepository<NotificationPinEntity, NotificationPinId> {
-    Optional<NotificationPinEntity> findByPhoneNumberAndPin(String phoneNumber, String pin);
-    void deleteByPhoneNumberAndPin(String phoneNumber, String pin);
     Optional<NotificationPinEntity> findByPhoneNumber(String phoneNumber);
 }
