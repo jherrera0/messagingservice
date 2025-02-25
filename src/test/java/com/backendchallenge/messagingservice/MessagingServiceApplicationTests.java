48f44e0c0dc6b53b7aa6c7a989564061ad711dd7
Jose Herrera <jose.herrera@pragma.com.co>
2025-02-25 16:25:11 -0500

    correct dependency to feedback automatic

--- src/test/java/com/backendchallenge/messagingservice/MessagingServiceApplicationTests.java
+++ src/test/java/com/backendchallenge/messagingservice/MessagingServiceApplicationTests.java
@@ -1,8 +1,6 @@
 package com.backendchallenge.messagingservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
